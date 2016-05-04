fed4d3b6531b1deb508a26ea9d4eb9bdaf6e995e
jamespedwards42 <james.p.edwards42@gmail.com>
2016-05-04 03:20:08 -0700

    Use same image for all tests.

--- scripts/rmRedis.sh
+++ scripts/rmRedis.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 docker ps -a -q -f name="$1" | xargs docker rm -f
 
--- scripts/startRedis.sh
+++ scripts/startRedis.sh
@@ -3,21 +3,16 @@
 name=$1
 serverPort=$2
 
-nodePort=$3
+startPort=$3
 numNodes=$4
+endPort=$((startPort + numNodes - 1))
 
 docker ps -a -q -f name="$name" | xargs docker rm -f
 
-docker run --name="$name"-"$serverPort" -d --publish="$serverPort":"$serverPort" redis:alpine redis-server --port "$serverPort" --requirepass 42 --save \"\" --repl-diskless-sync yes &
+docker run --name="$name"-"$serverPort" -d --publish="$serverPort":"$serverPort" jamespedwards42/alpine-redis-testing:unstable redis-server "$serverPort" 1 --requirepass 42 --save \"\" --repl-diskless-sync yes &
 
-nodePortC=$nodePort
-publish=""
-for ((i = 0; i < numNodes; i++, nodePortC++)); do
-   publish="$publish --publish=$nodePortC:$nodePortC"
-done
-
-docker run -d --name="$name-cluster" $publish jamespedwards42/alpine-redis-testing:unstable redis-server "$nodePort" "$numNodes" --cluster-announce-ip 127.0.0.1
+docker run -d --name="$name-cluster" -p "$startPort-$endPort:$startPort-$endPort" jamespedwards42/alpine-redis-testing:unstable redis-server "$startPort" "$numNodes" --cluster-enabled yes --cluster-announce-ip '127.0.0.1'
 
 wait
 
-exit 0 
+exit 0
