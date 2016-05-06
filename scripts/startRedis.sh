7b0311f8eb5ce25511d5696d5a29cd714f1cc2ee
jamespedwards42 <james.p.edwards42@gmail.com>
2016-05-05 23:32:44 -0700

    Move redis args from entrypoint to bash runner.

--- scripts/startRedis.sh
+++ scripts/startRedis.sh
@@ -11,9 +11,9 @@ announceIp=$5
 
 docker ps -a -q -f name="$name" | xargs docker rm -f
 
-docker run --name="$name"-"$serverPort" -d --publish="$serverPort":"$serverPort" jamespedwards42/alpine-redis-testing:unstable "$serverPort" 1 --requirepass 42 --save \"\" --repl-diskless-sync yes --appendfsync no --activerehashing no &
+docker run --name="$name"-"$serverPort" -d --publish="$serverPort":"$serverPort" jamespedwards42/alpine-redis-testing:unstable "$serverPort" 1 --requirepass 42 --protected-mode no --save \"\" --repl-diskless-sync yes --appendfsync no --activerehashing no &
 
-docker run -d --name="$name-cluster" -p "$startPort-$endPort:$startPort-$endPort" jamespedwards42/alpine-redis-testing:unstable "$startPort" "$numNodes" --cluster-enabled yes --appendfsync no --activerehashing no --cluster-announce-ip "$announceIp"
+docker run -d --name="$name-cluster" -p "$startPort-$endPort:$startPort-$endPort" jamespedwards42/alpine-redis-testing:unstable "$startPort" "$numNodes" --protected-mode no --save \"\" --repl-diskless-sync yes --cluster-enabled yes --appendfsync no --activerehashing no --cluster-announce-ip "$announceIp"
 
 wait
 
