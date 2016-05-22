91532038b698d1ffcd870624789310d8e9aa4de7
jamespedwards42 <james.p.edwards42@gmail.com>
2016-05-22 15:54:55 -0700

    Add module for integ testing.

---
+++ redis/modules/integ.c
@@ -0,0 +1,22 @@
+#include "../redismodule.h"
+
+int GetDb_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+
+    RedisModule_ReplyWithLongLong(ctx, RedisModule_GetSelectedDb(ctx));
+
+    return REDISMODULE_OK;
+}
+
+
+int RedisModule_OnLoad(RedisModuleCtx *ctx) {
+
+   if (RedisModule_Init(ctx, "integ", 1, REDISMODULE_APIVER_1) == REDISMODULE_ERR) {
+      return REDISMODULE_ERR;
+   }
+
+   if (RedisModule_CreateCommand(ctx, "integ.getdb", GetDb_RedisCommand, "readonly", 0, 0, 0) == REDISMODULE_ERR) {
+      return REDISMODULE_ERR;
+   }
+
+   return REDISMODULE_OK;
+}
