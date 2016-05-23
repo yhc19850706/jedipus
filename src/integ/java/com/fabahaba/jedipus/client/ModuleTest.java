bd6361864d21eb7070b16ee149e7f17caf6d3b8b
jamespedwards42 <james.p.edwards42@gmail.com>
2016-05-22 17:54:06 -0700

    cleanup module test. [skip ci]

--- src/integ/java/com/fabahaba/jedipus/client/ModuleTest.java
+++ src/integ/java/com/fabahaba/jedipus/client/ModuleTest.java
@@ -6,9 +6,12 @@ import org.junit.Test;
 
 import com.fabahaba.jedipus.cmds.Cmd;
 import com.fabahaba.jedipus.cmds.Cmds;
+import com.fabahaba.jedipus.cmds.PrimCmd;
 import com.fabahaba.jedipus.cmds.RESP;
 
 public class ModuleTest extends BaseRedisClientTest {
+
+  private static final PrimCmd INTEG_GETDB = Cmd.create("INTEG.GETDB").prim();
 
   @Test
   public void testModuleLoadAndCall() {
@@ -17,11 +20,13 @@ public class ModuleTest extends BaseRedisClientTest {
 
       final String reply = client.sendCmd(Cmds.MODULE, Cmds.MODULE_LOAD, "/redis/modules/integ.so");
       assertEquals(RESP.OK, reply);
+
       client.sendCmd(Cmds.SELECT, "1");
-      long db = client.sendCmd(Cmd.create("INTEG.GETDB").prim());
+      long db = client.sendCmd(INTEG_GETDB);
       assertEquals(1L, db);
+
       client.sendCmd(Cmds.SELECT, "0");
-      db = client.sendCmd(Cmd.create("INTEG.GETDB").prim());
+      db = client.sendCmd(INTEG_GETDB);
       assertEquals(0L, db);
     }
   }
